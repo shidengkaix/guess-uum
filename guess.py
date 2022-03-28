c6cb27bf63955718ab8d724c222bbfaa8720e706
dengkaix <shicpblmlb@gmail.com>
2022-03-28 17:03:22 +0800

    basic version

--- guess.py
+++ guess.py
@@ -1,5 +1,8 @@
 import random
-r = random.randint(1, 100)
+start = int(input('請使用者輸入開始值:'))
+end = int(input('請使用者輸入結束值:'))
+
+r = random.randint(start, end)
 count = 0
 while True:
 	count = count + 1
